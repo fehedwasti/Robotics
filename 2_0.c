ee16109a55b01a0f259129776c01b4bcc9eac8ad
Yun5141 <yun.fang.17@ucl.ac.uk>
2018-01-22 12:11:27 -0800

    2_0Code

---
+++ 2_0.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    //input number of elements
+    int n=0;    
+    printf("Please enter number of elements:");
+    scanf("%d",&n);
+
+    //input the elements
+    int numList[n];
+    printf("\nEnter %d integers:",n);    
+    for(int i=0;i<n;i++)
+    scanf("%d",&numList[i]);
+
+    //sorting the elements
+    int t;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(numList[j]>numList[j+1])
+			{
+				t=numList[j];
+				numList[j]=numList[j+1];
+				numList[j+1]=t;
+			}
+        }
+    }
+
+    //output the sorted elements
+    printf("Sorted list in ascending order:\n");
+    for(int i=0;i<n;i++)
+    printf("%d\t",numList[i]);
+
+    printf("\n");
+
+    return 0;
+}
