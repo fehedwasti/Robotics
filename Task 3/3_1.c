352b3b062ce853453cfdffb59734c11b047466b9
Yun5141 <yun.fang.17@ucl.ac.uk>
2018-02-01 09:03:47 -0800

    3_1angles

--- Task 3/3_1.c
+++ Task 3/3_1.c
@@ -4,6 +4,8 @@
 #include "abdrive.h"
 
 int counter=0;
+int angles[50];
+int inDex=0;
 
 void detectWall()
 {
@@ -15,12 +17,18 @@ void detectWall()
         if(counter==0)
         {
             drive_goto(15,-15);
+            angles[inDex]=15;
+            inDex++;
             counter++;
         }
         else if(counter==1)
         {
             drive_goto((int)(dist-5)*10/3.25,(int)(dist-5)*10/3.25);
             counter++;
+            for(int i=0;i<inDex;i++)
+            {
+                drive_goto(-angles[i],angles[i]);
+            }
             return;
         }
     }
@@ -47,6 +55,8 @@ void moveAlongwall()
         if(((irRight==0)&&(irLeft==0))||((irRight==1)&&(irLeft==1)))
         {
             drive_goto(-8,8);
+            angles[inDex]=-8;
+            inDex++;
         }
         else
         {
@@ -63,6 +73,8 @@ void moveAlongwall()
 int main()                                      // main function
 {
     drive_goto(5,-5);
+    angles[inDex]=5;
+    inDex++;
     //int dist=ping_cm(8);
     //printf("distance=%d\n",dist);
     drive_ramp(28,28);
