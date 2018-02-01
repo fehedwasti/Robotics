c7c34eebe0ca3604bc93babe516a7ed4473c1d6c
Yun5141 <yun.fang.17@ucl.ac.uk>
2018-02-01 08:39:50 -0800

    3_1

--- Task 3/3_1.c
+++ Task 3/3_1.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
+#include "ping.h"
 #include "simpletools.h"
 #include "abdrive.h"
-#include "simulator.h"
-#include "functions.c"
-#include "ping.h"
 
-int main(void){
-  turn(90,1);
-  int distance = ping_cm(8);
-  printf("%i", distance);
-  if (distance < 10){
-    drive_goto(-20,-20);
-  }
+int counter=0;
+
+void detectWall()
+{
+    int dist=ping_cm(8);
+    printf("distance=%d\n",dist);
+
+    if(dist<20)
+    {
+        if(counter==0)
+        {
+            drive_goto(15,-15);
+            counter++;
+        }
+        else if(counter==1)
+        {
+            drive_goto((int)(dist-5)*10/3.25,(int)(dist-5)*10/3.25);
+            counter++;
+            return;
+        }
+    }
+    else
+    return;
+}
+
+void moveAlongwall()
+{
+    int irLeft,irRight;
+    low(26);                                      // D/A0 & D/A1 to 0 V
+    low(27);
+
+    while(counter<2)                                      
+    {
+        freqout(11, 1, 38000);                      
+        irLeft = input(10);                         
+
+        freqout(1, 1, 38000);                       
+        irRight = input(2);
+
+        printf("%cirRight=%d\tirLeft=%d\n",HOME,irRight,irLeft);
+        
+        if(((irRight==0)&&(irLeft==0))||((irRight==1)&&(irLeft==1)))
+        {
+            drive_goto(-8,8);
+        }
+        else
+        {
+            //drive_goto(-irLeft*10+irRight*10,-irRight*10+irLeft*10);
+            drive_goto(23,23);
+        }
+        
+        detectWall();
+
+        pause(10);                                 
+    }
+}
+
+int main()                                      // main function
+{
+    drive_goto(5,-5);
+    //int dist=ping_cm(8);
+    //printf("distance=%d\n",dist);
+    drive_ramp(28,28);
+    moveAlongwall();
 }
